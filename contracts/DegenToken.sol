699656351387653a671f0e956b4bce558565b8f9
Sain Biswas <131385862+Sain-Biswas@users.noreply.github.com>
2024-07-06 22:05:00 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
     event LogMessage(string message);
-    constructor() ERC20("Degen", "DGN") {}
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
 
     string public redeemOptions =
-        "1. Weapon Skin\n 2. Primogems\n 3. Character Skin";
+        "1. Weapon Skin: 10 token \n 2. Primogems: 20 tokens \n 3. Character Skin: 30 tokens";
+    
+    mapping (address => uint[3]) items;
 
     //Minting new tokens
     function mint(address to, uint amount) public onlyOwner {
@@ -30,11 +33,15 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
         uint256 amount;
 
         if (itemNo == 1) {
-            amount = 1;
+            amount = 10;
+            items[msg.sender][0] += 1;
+
         } else if (itemNo == 2) {
-            amount = 2;
+            amount = 20;
+            items[msg.sender][1] += 1;
         } else if (itemNo == 3) {
-            amount = 3;
+            amount = 30;
+            items[msg.sender][2] += 1;
         } else {
             emit LogMessage("Redemption was UN-Successful");
             return;
@@ -42,6 +49,10 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
         _burn(msg.sender, amount);
         emit LogMessage("Redemption is Successful");
+    }
+
+    function getItems() view public returns (string[3] memory) {
+        return [ string.concat("Weapon Skin: " , Strings.toString(items[msg.sender][0]))  , string.concat("Primogems: " , Strings.toString(items[msg.sender][1])) , string.concat("Character Skin: " , Strings.toString(items[msg.sender][2])) ];
     }
 
     //Checking token balance
